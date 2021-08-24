a71aeebc1fa1fd982524ede88f7065071fa802c0
조경진 <cho881020@gmail.com>
2021-08-24 10:02:19 +0900

    프래그먼트의 kt 클래스 생성 + xml과 연결

---
+++ app/src/main/java/com/neppplus/fragment_20210824/fragments/FirstFragment.kt
@@ -0,0 +1,31 @@
+package com.neppplus.fragment_20210824.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.fragment_20210824.R
+
+class FirstFragment  : Fragment() {
+
+//    xml + kt의 조합. => 연결을 어떻게?
+//    kt단에서 어떤 xml을 쓸지 연결. (전용 함수 존재)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+}
+
+
+
+
+
+
+
