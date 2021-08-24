3b68357a973f178c13ae2406fab9b0a902974b8c
조경진 <cho881020@gmail.com>
2021-08-24 10:07:23 +0900

    프래그먼트 내부에서의 이벤트 처리

--- app/src/main/java/com/neppplus/fragment_20210824/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20210824/fragments/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.neppplus.fragment_20210824.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20210824.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment  : Fragment() {
 
@@ -18,6 +21,20 @@ class FirstFragment  : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+//    동작 관련 코드들이 작성되는 함수. => 액티비티 : onCreate
+//    이 클래스 : 화면 X, 화면에 올라가는 부품 O
+//    프래그먼트의 onCreate는 화면도 준비되기 전 => 동작시키면 에러 소지가 높다.
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        logBtn.setOnClickListener {
+            Log.d("부품에서 찍기", "로그버튼 눌림")
+        }
+
     }
 
 
